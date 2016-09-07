eb635f02eb9fd2a6da84da1991e6582484536959
chenyongming <chenyongming@yaochufa.com>
2016-09-07 11:26:55 +0800

    去除swoole_http_server::setGlobal()，1.8.6版本移除了改函数

--- app/configs/services.php
+++ app/configs/services.php
@@ -15,57 +15,47 @@ $loader = new Loader();
 //     __DIR__ . '/../models/',
 // ))->register();
 
-
 $loader->registerNamespaces(
-        array(
-                'MyApp\Controllers' => __DIR__ . '/../controllers/',
-                'MyApp\Controllers\Front' => __DIR__ . '/../controllers/front'
-        )
+    array(
+        'MyApp\Controllers'       => __DIR__ . '/../controllers/',
+        'MyApp\Controllers\Front' => __DIR__ . '/../controllers/front',
+    )
 )->register();
 
-//try {
+//引入配置文件
+$config = include_once APP_PATH . "/app/configs/config.php";
 
-    //引入配置文件
-    $config = include_once APP_PATH . "/app/configs/config.php";
+// Create a DI
+$di = new FactoryDefault();
 
-    // Create a DI
-    $di = new FactoryDefault();
+$di->setShared('config', function () use ($config) {
+    return $config;
+});
 
-    $di->setShared('config', function () use ($config) {
-        return $config;
-    });
+// Setup the router component
+$di->set('router', function () {
 
-    // Setup the router component
-    $di->set('router', function () {
+    return require __DIR__ . '/routers.php';
+}, true);
+// Setup the view component
+$di->set('view', function () {
+    $view = new View();
+    $view->setViewsDir('../app/views/');
+    return $view;
+});
 
-    	return require __DIR__ . '/routers.php';
-    },true);
-    // Setup the view component
-    $di->set('view', function () {
-        $view = new View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    });
+// Setup a base URI so that all generated URIs include the "tutorial" folder
+$di->set('url', function () {
+    $url = new UrlProvider();
+    $url->setBaseUri('/');
+    return $url;
+});
+$di->setShared('logger', function () use ($config) {
 
-    // Setup a base URI so that all generated URIs include the "tutorial" folder
-    $di->set('url', function () {
-        $url = new UrlProvider();
-        $url->setBaseUri('/');
-        return $url;
-    });
-    $di->setShared('logger', function () use ($config) {
-
-        return new FileAdapter($config->logger->application);
-    });
-    $di->set('dispatcher', function(){
-    	$dispatcher = new Dispatcher();
-    	$dispatcher->setDefaultNamespace('MyApp\Controllers');
-    	return $dispatcher;
-    });
-/*}catch(\Phalcon\Exception $e) {
-    echo '<pre>' . get_class($e), ": ", $e->getMessage(), "<br>";
-    echo " File=", $e->getFile(), "<br>";
-    echo " Line=", $e->getLine(), "<br>";
-    echo " Trace=", $e->getTraceAsString() . '</pre>';
-
-}*/
+    return new FileAdapter($config->logger->application);
+});
+$di->set('dispatcher', function () {
+    $dispatcher = new Dispatcher();
+    $dispatcher->setDefaultNamespace('MyApp\Controllers');
+    return $dispatcher;
+});
--- public/server.php
+++ public/server.php
@@ -2,125 +2,120 @@
 
 /**
  * Swoole HttpServer 服务器
- * @author Yeoman 
+ * @author Yeoman
  * @since 2016-09-05
  *
  */
 //define('APP_PATH', realpath('..'));
-define('APP_PATH', realpath(dirname(__FILE__).'/../'));
+define('APP_PATH', realpath(dirname(__FILE__) . '/../'));
 
 class HttpServer
 {
 
-	public static $instance;
-	private $http;
-	private $application;
-	
-	public function __construct(){
-		// 创建swoole_http_server对象
-		$this->http = new swoole_http_server('0.0.0.0',9501);
-		//设置参数
-		$this->http->set(
-				array(
-					'worker_num' => 16,
-					'deamonize' => false,
-					'max_request' => 10000,
-					'dispatch_mode' => 1
-				)
-			);
-		$this->http->setGlobal(HTTP_GLOBAL_ALL);
-		// 绑定WorkerStart
-		$this->http->on('WorkerStart',array($this,'onWorkStart'));
-		// 绑定request
-		$this->http->on('request',array($this,'onRequest'));
-		// 开启服务器
-		$this->http->start();
+    public static $instance;
+    private $http;
+    private $application;
 
-	}
-	// WorkStart 回调
-	public function onWorkStart()
-	{
-		require APP_PATH . '/app/configs/services.php';
-		$this->application = new \Phalcon\Mvc\Application;
-		$this->application->setDI($di);
-	}
-	//处理http请求
-	public function onRequest($request,$response){
-		//注册捕获错误函数
-		//register_shutdown_function('handleFatal');
-		register_shutdown_function(array($this, 'handleFatal'));
-		ob_start();
-		try{
-			$_GET['_url'] = $request->server['request_uri'];
-			$this->application->logger->info('Http Get: ' . json_encode($_GET['_url']));
-			echo $this->application->handle()->getContent();
-		} catch (Exception $e){
-			echo $e->getMessage();
-		}
-		$result = ob_get_contents();
-		ob_end_clean();
-		$response->end($result);
-	}
-	//获取实例对象
-	public static function getInstance(){
-		if(!self::$instance) {
+    public function __construct()
+    {
+        // 创建swoole_http_server对象
+        $this->http = new swoole_http_server('0.0.0.0', 9501);
+        //设置参数
+        $this->http->set(
+            array(
+                'worker_num'    => 16,
+                'deamonize'     => false,
+                'max_request'   => 10000,
+                'dispatch_mode' => 1,
+            )
+        );
+        //$this->http->setGlobal(HTTP_GLOBAL_ALL);
+        // 绑定WorkerStart
+        $this->http->on('WorkerStart', array($this, 'onWorkStart'));
+        // 绑定request
+        $this->http->on('request', array($this, 'onRequest'));
+        // 开启服务器
+        $this->http->start();
 
-			self::$instance = new HttpServer();
-		} 
+    }
+    // WorkStart 回调
+    public function onWorkStart()
+    {
+        require APP_PATH . '/app/configs/services.php';
+        $this->application = new \Phalcon\Mvc\Application;
+        $this->application->setDI($di);
+    }
+    //处理http请求
+    public function onRequest($request, $response)
+    {
+        //注册捕获错误函数
+        //register_shutdown_function('handleFatal');
+        register_shutdown_function(array($this, 'handleFatal'));
+        ob_start();
+        try {
+            $_GET['_url'] = $request->server['request_uri'];
+            //$this->application->logger->info('Http Get: ' . json_encode($_GET['_url']));
+            echo $this->application->handle()->getContent();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        $result = ob_get_contents();
+        ob_end_clean();
+        $response->end($result);
+    }
+    //获取实例对象
+    public static function getInstance()
+    {
+        if (!self::$instance) {
 
-		return self::$instance;
-	}
-	//捕获Server运行期致命错误
-	public function handleFatal()
-	{
-	    $error = error_get_last();
-	    if (isset($error['type']))
-	    {
-	        switch ($error['type'])
-	        {
-	        case E_ERROR :
-	        case E_PARSE :
-	        case E_CORE_ERROR :
-	        case E_COMPILE_ERROR :
-	            $message = $error['message'];
-	            $file = $error['file'];
-	            $line = $error['line'];
-	            $log = "$message ($file:$line)\nStack trace:\n";
-	            $trace = debug_backtrace();
-	            foreach ($trace as $i => $t)
-	            {
-	                if (!isset($t['file']))
-	                {
-	                    $t['file'] = 'unknown';
-	                }
-	                if (!isset($t['line']))
-	                {
-	                    $t['line'] = 0;
-	                }
-	                if (!isset($t['function']))
-	                {
-	                    $t['function'] = 'unknown';
-	                }
-	                $log .= "#$i {$t['file']}({$t['line']}): ";
-	                if (isset($t['object']) and is_object($t['object']))
-	                {
-	                    $log .= get_class($t['object']) . '->';
-	                }
-	                $log .= "{$t['function']}()\n";
-	            }
-	            if (isset($_SERVER['REQUEST_URI']))
-	            {
-	                $log .= '[QUERY] ' . $_SERVER['REQUEST_URI'];
-	            }
-	            //error_log($log);
-	            //$serv->send($this->currentFd, $log);
-	            $this->application->logger->info('error log: ' . $log);
-	            $this->response->end($this->currentFd.'_'.$log);
-	        default:
-	            break;
-	        }
-	    }
-	}
+            self::$instance = new HttpServer();
+        }
+
+        return self::$instance;
+    }
+    //捕获Server运行期致命错误
+    public function handleFatal()
+    {
+        $error = error_get_last();
+        if (isset($error['type'])) {
+            switch ($error['type']) {
+                case E_ERROR:
+                case E_PARSE:
+                case E_CORE_ERROR:
+                case E_COMPILE_ERROR:
+                    $message = $error['message'];
+                    $file    = $error['file'];
+                    $line    = $error['line'];
+                    $log     = "$message ($file:$line)\nStack trace:\n";
+                    $trace   = debug_backtrace();
+                    foreach ($trace as $i => $t) {
+                        if (!isset($t['file'])) {
+                            $t['file'] = 'unknown';
+                        }
+                        if (!isset($t['line'])) {
+                            $t['line'] = 0;
+                        }
+                        if (!isset($t['function'])) {
+                            $t['function'] = 'unknown';
+                        }
+                        $log .= "#$i {$t['file']}({$t['line']}): ";
+                        if (isset($t['object']) and is_object($t['object'])) {
+                            $log .= get_class($t['object']) . '->';
+                        }
+                        $log .= "{$t['function']}()\n";
+                    }
+                    if (isset($_SERVER['REQUEST_URI'])) {
+                        $log .= '[QUERY] ' . $_SERVER['REQUEST_URI'];
+                    }
+                    //error_log($log);
+                    //$serv->send($this->currentFd, $log);
+                    $this->application->logger->info('error log: ' . $log);
+                    $this->response->end($this->currentFd . '_' . $log);
+                default:
+                    break;
+            }
+        }
+    }
 }
 
 HttpServer::getInstance();
